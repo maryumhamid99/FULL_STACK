cfc236e3044cddf266c3c4e9ce1381b5d89b7d3b
maryumhamid99 <maryumhamid2010@hotmail.com>
2022-07-05 21:58:03 +0300

    Task 2.9 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' , number: '39-44-5323523' }])
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' , number: '39-44-5323523' },{ name: 'Ada Lovelace', number: '39-44-5323523' }, { name: 'Dan Abramov', number: '12-43-234345' }, { name: 'Mary Poppendieck', number: '39-23-6423122' }])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
@@ -12,7 +12,6 @@ const App = () => {
         alert(`${newName} is already added to phonebook.`);
         return
       }
-      setPersons( persons.concat( {name: newName} ))
       setPersons( persons.concat( {name: newName, number: newNumber} ))
       setNewName("")
       setNewNumber("")
@@ -20,10 +19,20 @@ const App = () => {
 
     const handleNewNameChange = (event) => setNewName(event.target.value)
     const handleNewNumberChange = (event) => setNewNumber(event.target.value)
+    const handleFilterChange = (event) => {
+      setFilterName(event.target.value)
+    }
+    const personsShow = persons.filter( person => person.name.toLowerCase().startsWith(filter.toLowerCase()) )
+
+
 
   return (
       <div>
         <h2>Phonebook</h2>
+        <div>
+        filter shown with<input value={filterName} onChange={handleFilterChange}/>
+        </div>
+        <h3>add a new</h3>
         <form>
           <div>
             name: <input value={newName} onChange={handleNewNameChange}/>
@@ -36,7 +45,7 @@ const App = () => {
           </div>
         </form>
         <h2>Numbers</h2>
-          {persons.map(person => <div  key={person.name} >{person.name} {person.number}</div>)}
+          {personsShow.map(person => <div  key={person.name} >{person.name} {person.number}</div>)}
 
       </div>
   )
