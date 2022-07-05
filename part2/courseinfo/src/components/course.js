e6b151af7bae8633e81ba627997294a3b0cf355e
maryumhamid99 <maryumhamid2010@hotmail.com>
2022-07-05 21:25:23 +0300

    Task 2.3 completed

--- part2/courseinfo/src/components/course.js
+++ part2/courseinfo/src/components/course.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './header'
 import Content from './content'
+import Total from './total'
 
 const Course = (props) => {
     return(
         <>
             <Header name = {props.course.name} />
             <Content parts = {props.course.parts} />
+            <Total course = {props.course}/>
         </>
     )
 
