323c6f9c013c878159dc42b551177ddf88721a6e
maryumhamid99 <maryumhamid2010@hotmail.com>
2022-07-05 21:41:29 +0300

    Task 2.7 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,6 +6,10 @@ const App = () => {
 
     const addName = (event) => {
       event.preventDefault()
+      if (persons.filter( person => person.name === newName ).length > 0){
+        alert(`${newName} is already added to phonebook.`);
+        return
+      }
       setPersons( persons.concat( {name: newName} ))
       setNewName("")
     }
