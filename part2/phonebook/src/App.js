6c9540ed4db66d7cd2c12446b32969035885e0e7
maryumhamid99 <maryumhamid2010@hotmail.com>
2022-07-06 21:32:22 +0300

    Task 2.20 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,12 +4,14 @@ import React, { useState , useEffect} from 'react'
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
-
+import Notification from "./components/notification";
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' , number: '39-44-5323523' },{ name: 'Ada Lovelace', number: '39-44-5323523' }, { name: 'Dan Abramov', number: '12-43-234345' }, { name: 'Mary Poppendieck', number: '39-23-6423122' }])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const data_url = 'http://localhost:3001/persons'
+  const [message, setMessage] = useState(null)
+  const [error, setError] = useState(null)
 
   const addName = (event) => {
     event.preventDefault()
@@ -17,13 +19,24 @@ const App = () => {
       const duplicates = persons.filter(p => p.name === newName)
       if (
           duplicates.length >= 1 &&
-          window.confirm(`${duplicates[0].name} is already added to phonebook, replace the old number with a new one?`)
+          window.confirm(`${duplicates[0].name} is already added to phonebook, replace?`)
       ){
           phonebookService.updateEntry(duplicates[0].id, data)
-              .then( updatedPerson => setPersons( persons.map( p => p.id === duplicates[0].id ? updatedPerson : p ) ) )
-      }else {
+          .then( updatedPerson => {
+            if (updatedPerson == null){
+                setNotification(`Information: ${duplicates[0].name} has been removed already from the server`, true)
+                return
+            }
+            console.log("updating entry.....")
+            setPersons( persons.map( p => p.id === duplicates[0].id ? updatedPerson : p ) )
+            setNotification(`Updated number of ${updatedPerson.name}`, false)
+        })
+        }else {
           phonebookService.addEntry( data )
-              .then( newPerson => setPersons( persons.concat(newPerson) ) )
+          .then( newPerson => {
+            setPersons( persons.concat(newPerson) )
+            setNotification(`Added ${newPerson.name}`, false)
+        })
       }
 
       phonebookService.addEntry( {name: newName, number: newNumber} )
@@ -57,6 +70,8 @@ const App = () => {
   return (
       <div>
         <h2>Phonebook</h2>
+        <Notification message={message} isError={false} />
+            <Notification message={error} isError={true} />
         <Filter filter={filter} changeHandler={handleFilterChange}/>
 
         <h3>add a new</h3>
--- part2/phonebook/src/server.js
+++ part2/phonebook/src/server.js
@@ -20,6 +20,7 @@ const deleteEntry = (id) => {
 const updateEntry = (id, person) => {
     return axios.put(`${baseUrl}/${id}`, person)
         .then(response => response.data)
+        .catch( error => console.log("Failed to delete") )
 }
 const exports = {addEntry, getAll, deleteEntry,updateEntry}
 export default exports
