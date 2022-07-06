270ba6682c8b7daa8b30f97651fba3677dcae478
maryumhamid99 <maryumhamid2010@hotmail.com>
2022-07-06 20:09:38 +0300

    Task 2.14 completed

--- part2/data-for-countries/src/components/country.js
+++ part2/data-for-countries/src/components/country.js
@@ -1,3 +1,6 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
 const Button = (props) => {
     const { countryDisp, showFlag, name } = props
   
@@ -23,7 +26,26 @@ const Button = (props) => {
   
   const Country = (props) => {
     const { country, countryDisp, showFlag } = props
-  
+    
+    const api_key = process.env.REACT_APP_API_KEY
+    const weather_api = `http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}`
+        
+    const [windSpeed, setWindSpeed] = useState('')
+    const [temperature, setTemperature] = useState('')
+    const [weatherIcon, setWeatherIcon] = useState('')
+
+
+
+    useEffect(() => {
+        axios.get(weather_api).then(response => {
+          setWeatherIcon(response.data['current']['weather_icons'][0])
+          setTemperature(response.data['current']['temperature'])
+          setWindSpeed(response.data['current']['wind_speed'])
+        })
+      }, [weather_api])
+
+
+
     if (countryDisp[country.name]) {
       return (
         <div>
@@ -42,6 +64,25 @@ const Button = (props) => {
           <div>
             <img src={country.flag} alt="Country Flag" width="6%" />
           </div>
+        
+          <div>
+          <h4>Weather in {country.capital}</h4>
+          <div>
+            <div>
+              <h4>Temperature: </h4>
+              <div>
+                {temperature} Celcius.
+              </div>
+              <img src={weatherIcon} alt="Weather Icon" width="3%"  height="3%"/>
+            </div>
+            <div>
+              <h4>Wind: </h4>
+              <div>Speed: {windSpeed} mph</div>
+            </div>
+          </div>
+        </div>
+
+
         </div>
       )
     }
