3b8131dbc43f51ecaf48dce5fcc1cc90cb1e64f2
maryumhamid99 <maryumhamid2010@hotmail.com>
2022-07-20 16:59:58 +0300

    task 4.2

--- part4/index.js
+++ part4/index.js
@@ -1,46 +1,31 @@
-require('dotenv').config()
 const mongoose = require('mongoose')
 const express = require('express')
 const http = require('http')
 const app = express()
 const cors = require('cors')
 
-const schema = new mongoose.Schema({
-  author: String,
-  title: String,
-  url: String,
-  likes: Number
-})
+const blogRouter = require('./controllers/blog')
+const middleware = require('./utils/middleware')
 
-const Blog = mongoose.model('Blog', schema)
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const mongoUrl = process.env.MONGODB_URI
-const PORT = process.env.PORT
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
   })
 
-  
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use(cors())
+app.use(middleware.requestLogger)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+app.use(express.json())
+app.use('/api/blogs', blogRouter)
 
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
