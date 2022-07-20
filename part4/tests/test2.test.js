f9c8086f74e6ef036c4031a738106bc9739b6709
maryumhamid99 <maryumhamid2010@hotmail.com>
2022-07-20 18:52:14 +0300

    task 4.5

--- part4/tests/test2.test.js
+++ part4/tests/test2.test.js
@@ -1,75 +1,76 @@
 const listHelper = require('../utils/list_helper')
 
+
+const listWithOneBlog = [
+{
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+}
+]
+
+const listWithMultipleBlogs = [
+{
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+},
+{
+    _id: "5a422a851b54a676234d17f7",
+    title: "Go to somethign",
+    author: "maryum hamid",
+    url: "https://google.com/",
+    likes: 4,
+    __v: 0
+},
+{
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "go to 1",
+    author: "maryum 1",
+    url: "https://google.com/",
+    likes: 1,
+    __v: 0
+},
+{
+    _id: "5a422b891b54a676234d17fa",
+    title: "Go to somethign",
+    author: "maryum hamid",
+    url: "https://google.com/",
+    likes: 4,
+    __v: 0
+},
+{
+    _id: "5a422ba71b54a676234d17fb",
+    title: "Go to somethign",
+    author: "maryum hamid",
+    url: "https://google.com/",
+    likes: 4,
+    __v: 0
+},
+{
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Go to somethign",
+    author: "maryum hamid",
+    url: "https://google.com/",
+    likes: 4,
+    __v: 0
+}  
+]
+
 test('dummy returns one', () => {
-  const blogs = []
-
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
-})
-
-describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    }
-  ]
-
-  const listWithMultipleBlogs = [
-    {
-        _id: "5a422aa71b54a676234d17f8",
-        title: "Go To Statement Considered Harmful",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-        likes: 5,
-        __v: 0
-    },
-    {
-      _id: "5a422a851b54a676234d17f7",
-      title: "Go to somethign",
-      author: "maryum hamid",
-      url: "https://google.com/",
-      likes: 4,
-      __v: 0
-    },
-    {
-      _id: "5a422b3a1b54a676234d17f9",
-      title: "go to 1",
-      author: "maryum 1",
-      url: "https://google.com/",
-      likes: 1,
-      __v: 0
-    },
-    {
-      _id: "5a422b891b54a676234d17fa",
-      title: "Go to somethign",
-      author: "maryum hamid",
-      url: "https://google.com/",
-      likes: 4,
-      __v: 0
-    },
-    {
-      _id: "5a422ba71b54a676234d17fb",
-      title: "Go to somethign",
-      author: "maryum hamid",
-      url: "https://google.com/",
-      likes: 4,
-      __v: 0
-    },
-    {
-      _id: "5a422bc61b54a676234d17fc",
-      title: "Go to somethign",
-      author: "maryum hamid",
-      url: "https://google.com/",
-      likes: 4,
-      __v: 0
-    }  
-  ]
-
+    const blogs = []
+  
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
+  })
+  
+  describe('total likes: ', () => {
   test('when list has only one blog, equals the likes of that', () => {
     const result = listHelper.countLikes(listWithOneBlog)
     expect(result).toBe(5)
@@ -77,6 +78,25 @@ describe('total likes', () => {
 
   test('when list has multiple blogs, equals the likes of that', () => {
     const result = listHelper.countLikes(listWithMultipleBlogs)
-    expect(result).toBe(36)
+    expect(result).toBe(22)
   })
+})
+
+
+describe('favorite blog', () => {
+    test('when list has only one blog, the most favorite blog is', () => {
+      const result = listHelper.favoriteBlog(listWithOneBlog)
+      expect(result).toEqual({
+        title: 'Go To Statement Considered Harmful',
+        likes: 5
+      })
+    })
+  
+    test('when list has multiple blogs, the most favorite blog is', () => {
+      const result = listHelper.favoriteBlog(listWithMultipleBlogs)
+      expect(result).toEqual({
+        title: "Go To Statement Considered Harmful",
+        likes: 5
+      })
+    })
 })
--- part4/utils/listHelper.js
+++ part4/utils/listHelper.js
@@ -5,6 +5,16 @@ const countLikes = (blogs) => {
     const reducer = (sum, blog) => { return sum + blog.likes }
     return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
 }
+
+const favoriteBlog = (blogs) => {
+    blogs.sort((blog1, blog2) => blog2.likes - blog1.likes)
+    const result = {
+        title: blogs[0].title,
+        likes: blogs[0].likes
+    }
+    return result
+}
+
 module.exports = {
-    dummy, countLikes
+    dummy, countLikes,favoriteBlog
 }
